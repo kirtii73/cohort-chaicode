567e6987bdf89f3fdcf440f41e26a228a5c685f7
Kirti Jirati <140043656+kirtii73@users.noreply.github.com>
2025-02-14 18:21:00 +0530

    conditional statements

---
+++ js/chapter-01.js
@@ -0,0 +1,12 @@
+function checkEvenOdd(num){
+    if(num%2==0){
+        console.log("even")
+    }
+    else{
+        console.log("odd")
+    }
+}
+checkEvenOdd(8)
+checkEvenOdd(7)
+checkEvenOdd(45689785478412479854)
+
---
+++ js/chapter-02.js
@@ -0,0 +1,13 @@
+let numberOfGuests = 4;
+
+let pizzaSize;
+
+if(numberOfGuests <= 2){
+    console.log( pizzaSize = "small")
+}
+else if (numberOfGuests <=5){
+    console.log(pizzaSize = "Medium")
+}
+else{
+    console.log(pizzaSize = "large")
+}
