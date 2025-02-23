a636c2aee05114ca845043b857393096838cfa0f
Kirti Jirati <140043656+kirtii73@users.noreply.github.com>
2025-02-23 22:54:27 +0530

    Update objects-01.js

--- js/objects-01.js
+++ js/objects-01.js
@@ -36,3 +36,53 @@ console.log("Person after deleting hobbies:", person);
 for (let key in person) {
     console.log(`${key}: ${person[key]}`);
 }
+
+
+
+const book = {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    yearPublished: 1925,
+    genres: ["Classic", "Novel", "Drama"],
+    publisher: {
+        name: "Scribner",
+        location: "New York"
+    },
+    getSummary: function() {
+        return `${this.title} by ${this.author}, published in ${this.yearPublished}`;
+    },
+    getAge: function(currentYear) {
+        return currentYear - this.yearPublished;
+    },
+    isClassic: function() {
+        return this.getAge(new Date().getFullYear()) > 50;
+    }
+};
+
+// Accessing object properties
+console.log("Book Title:", book.title);
+console.log("Author:", book.author);
+console.log("Year Published:", book.yearPublished);
+console.log("Genres:", book.genres.join(", "));
+console.log("Publisher Name:", book.publisher.name);
+console.log("Book Summary:", book.getSummary());
+console.log("Book Age:", book.getAge(2024));
+console.log("Is Classic?:", book.isClassic());
+
+// Modifying an object property
+book.yearPublished = 1926;
+console.log("Updated Year Published:", book.yearPublished);
+
+// Adding a new property
+book.pages = 180;
+console.log("Number of Pages:", book.pages);
+
+// Deleting a property
+delete book.genres;
+console.log("Book after deleting genres:", book);
+
+// Looping through object properties
+for (let key in book) {
+    console.log(`${key}:`, book[key]);
+}
+
